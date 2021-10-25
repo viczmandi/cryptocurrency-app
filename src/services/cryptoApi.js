b39243680e5facacb6da83e9d5afdc279bd3d8f4
viczmandi <viczmandi@gmail.com>
2021-10-26 00:00:52 +0200

    news added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoApiHeadres = {
+const cryptoApiHeaders = {
     'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
     'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({url, headers: cryptoApiHeadres});
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
